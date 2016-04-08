1d48d1af435343cfab92a1b58a3b69fe278ee3ea
hhj <hhwang@crscube.co.kr>
2016-04-08 14:52:13 +0900

    This is branch Test!

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World! I'm chaning the source NOW");
+        System.out.println("Hello, World! This is branch test");
     }
 
 }
